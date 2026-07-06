f72b19aed0a2e5d39692ede87b357082c8e68c14
unknown <abdulgeniabdulaziz@gmail.com>
2026-07-06 17:36:50 +0300

    Resolve merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
-=======
-  /* config options here */
->>>>>>> 310d1b85f6650eb9992b428c73310bfb174ef3f6
 };
 
 export default nextConfig;
